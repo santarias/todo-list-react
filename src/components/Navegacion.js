188fb77acb2005e0ef34dbde7600a2d0d66e1e8b
Santiago Arias <santyac27@gmail.com>
2021-03-06 12:05:01 +0100

    Se añade componente de navegacion

---
+++ src/components/Navegacion.js
@@ -0,0 +1,11 @@
+function Navegacion(props) {
+    return (
+        <nav className="navbar navbar-dark bg-dark" >
+            <a href="" className="text-white">
+                {props.titulo}
+            </a>
+        </nav>
+    );
+}
+
+export default Navegacion;
