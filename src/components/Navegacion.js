9a025fd50459f6dd21a817a7c6b8432afc4c7889
Santiago Arias <santyac27@gmail.com>
2021-03-07 18:15:49 +0100

    Cambio componente funcional a clase componente

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -1,14 +1,30 @@
-function Navegacion(props) {
+import { render } from "react-dom";
+import { Component } from "react";
+
+class Navegacion extends Component {
+
+  constructor(props) {
+    super(props);
+    this.titulo = props.titulo;
+    this.lista = props.lista;
+  }
+
+  handleClick() {
+    alert("algo");
+  }
+
+  render() {
     return (
-        <nav className="navbar navbar-dark bg-dark barra-navegacion" >
-            <a href="#" className="text-white">
-                {props.titulo}
-                <span className="badge badge-pill badge-light ml-2">
-                    {props.lista.length}
-                </span>
-            </a>
-        </nav>
+      <nav className="navbar navbar-dark bg-dark barra-navegacion" >
+        <a href="#" className="text-white">
+          {this.titulo}
+          <span className="badge badge-pill badge-light ml-2">
+            {this.lista.length}
+          </span>
+        </a>
+      </nav>
     );
+  }
 }
 
 export default Navegacion;
